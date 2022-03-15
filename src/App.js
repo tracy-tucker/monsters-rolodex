cfca23301af3d35098af2a3ba27436cf755bc2c3
Tracy Tucker <tracytuckeremail@gmail.com>
2022-03-14 19:30:56 -0500

    local state of monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: "Tracy",
+          id: "01",
+        },
+        {
+          name: "Troy",
+          id: "02",
+        },
+        {
+          name: "Preston",
+          id: "03",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster, index) => {
+          return <h1 key={index}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
