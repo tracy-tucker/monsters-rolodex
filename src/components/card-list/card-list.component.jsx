061587effe3c8264f785f50c06f22740d38c9e4d
Tracy Tucker <tracytuckeremail@gmail.com>
2022-03-16 18:40:09 -0500

    styling search-box and adding monster images

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,9 +6,19 @@ class CardList extends Component {
     const { monsters } = this.props;
     return (
       <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
